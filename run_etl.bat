59142cdbdcc8dfad0b121550f550820be9d4a804
lamdoanh2468 <lamdoanh2468@gmail.com>
2025-11-25 22:44:12 +0700

    fix load to staging

--- run_etl.bat
+++ run_etl.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d D:\LaptopDW
 
-:: Không cần set utf-8 nữa vì Python lo rồi, nhưng để cũng không sao
+:: Không cần set utf-8 nữa vì Python đã lo
 set PYTHONIOENCODING=utf-8
 
-:: Gọi Python chạy file Scheduler (Không cần >> log nữa)
+:: Gọi Python chạy file Scheduler.py
 "D:\LaptopDW\venv\Scripts\python.exe" "scripts\Scheduler.py"
